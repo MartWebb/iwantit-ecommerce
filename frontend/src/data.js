5d40f0040379c3ede69ec55dea53c0c2114e1e43
Martin <martwebb2@gmail.com>
2021-02-17 16:08:27 -0500

    initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,87 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Xbox series X',
+            image: '/images/xboxx.jpg',
+            description:
+                'Introducing Xbox Series X, the fastest, most powerful Xbox ever. Play thousands of titles from four generations of consoles-all games look and play best on Xbox Series X.',
+            brand: 'Microsoft',
+            category: 'Electronics',
+            price: 1499.99,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 12,
+        },
+        {
+            _id: '2',
+            name: 'Nintendo Switch',
+            image: '/images/switch.jpg',
+            description:
+                'Do what You never thought possible with Nintendo Switch. Go from single- and multi-player thrills at home, to playing the same title wherever and whenever you want.',
+            brand: 'Nintendo',
+            category: 'Electronics',
+            price: 399.99,
+            countInStock: 7,
+            rating: 5.0,
+            numReviews: 8,
+        },
+        {
+            _id: '3',
+            name: 'Sony Playstation 4',
+            image: '/images/playstation4.jpg',
+            description:
+            'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+            brand: 'Sony',
+            category: 'Electronics',
+            price: 399.99,
+            countInStock: 5,
+            rating: 0,
+            numReviews: 0,
+        },
+        {
+            _id: '4',
+            name: 'Sony Playstation 5',
+            image: '/images/playstation5.jpg',
+            description:
+                'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+            brand: 'Sony',
+            category: 'Electronics',
+            price: 1613.94,
+            countInStock: 11,
+            rating: 5,
+            numReviews: 12,
+        },
+        {
+            _id: '5',
+            name: 'Xbox One S 1TB Console',
+            image: '/images/xbox1.jpg',
+            description:
+                'Own the Xbox One S 1TB console and get access to over 2, 200 games that you can only play on Xbox One. This bundle includes a 1-month Xbox Game Pass ultimate trial.',
+            brand: 'Microsoft',
+            category: 'Electronics',
+            price: 449.99,
+            countInStock: 7,
+            rating: 3.5,
+            numReviews: 10,
+        },
+        {
+            _id: '6',
+            name: 'Oculus Quest 2',
+            image: '/images/oculas2.jpg',
+            description:
+                'Oculus quest 2 is our most advanced all-in-one VR system yet. Every detail has been engineered to make virtual worlds adapt to your movements, letting you explore awe-inspiring games and experiences with unparalleled freedom.',
+            brand: 'Facebook Technologies, LLC.',
+            category: 'Electronics',
+            price: 399.99,
+            countInStock: 0,
+            rating: 4,
+            numReviews: 12,
+        },
+    ]
+}
+  // common js
+  // module.exports = products;
+  // es modules
+  export default data;
+  
