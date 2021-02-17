eaf695a202e429c8799ac07a55b467af2cc47966
Martin <martwebb2@gmail.com>
2021-02-17 17:26:26 -0500

    added product page and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,30 @@
 // import logo from './logo.svg';
 // import './App.css';
-
-import data from "./data";
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
-  return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">I Want It</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-			{data.products.map(product => (
-				<div key={product._id} className="card">
-					<a href={`/product/${product._id}`}>
-						<img className="medium" src={product.image} alt={product.name}/>
-					</a>
-					<div className="card-body">
-						<a href={`/product/${product._id}`}>
-							<h2>{product.name}</h2>
-						</a>
-						<div className="rating">
-							<span> <i className="fa fa-star"></i> </span>
-							<span> <i className="fa fa-star"></i> </span>
-							<span> <i className="fa fa-star"></i> </span>
-							<span> <i className="fa fa-star"></i> </span>
-							<span> <i className="fa fa-star"></i> </span>
-						</div>
-						<div className="price">${product.price}</div>
-					</div>
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="row">
+				<div>
+					<a className="brand" href="/">I Want It</a>
 				</div>
-			))}
-            
-          </div>
-        </div>
-      </main>
-      <footer className="row center">All rights reserved</footer>
-    </div>
-  );
+				<div>
+					<a href="/cart">Cart</a>
+					<a href="/signin">Sign In</a>
+				</div>
+				</header>
+				<main>
+					<Route path="/product/:id" component={ProductPage}></Route>
+					<Route path="/" component={HomePage} exact></Route>
+				</main>
+				<footer className="row center">Copyright &copy; BuyIt</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
